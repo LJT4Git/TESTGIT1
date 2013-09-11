0c3a1e8c11e36a88212001eac110041aaee7b053
LJT4Git <2387566995@qq.com>
2013-09-11 10:38:11 +0800

    V9

--- TESTGit1/TESTGit1/Form4.Designer.cs
+++ TESTGit1/TESTGit1/Form4.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< HEAD
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
@@ -60,15 +61,44 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+=======
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(61, 70);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "btn2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(70, 130);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(41, 12);
+            this.lb1.TabIndex = 1;
+            this.lb1.Text = "label1";
+>>>>>>> 48d03dd54f272bc63c06c4c91f4f6815e11a167f
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+<<<<<<< HEAD
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+=======
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.btn2);
+>>>>>>> 48d03dd54f272bc63c06c4c91f4f6815e11a167f
             this.Name = "Form4";
             this.Text = "Form4";
             this.ResumeLayout(false);
@@ -78,8 +108,13 @@
 
         #endregion
 
+<<<<<<< HEAD
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+=======
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lb1;
+>>>>>>> 48d03dd54f272bc63c06c4c91f4f6815e11a167f
     }
 }
--- TESTGit1/TESTGit1/Form4.cs
+++ TESTGit1/TESTGit1/Form4.cs
@@ -16,9 +16,15 @@ namespace TESTGit1
             InitializeComponent();
         }
 
+<<<<<<< HEAD
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text = "world!";
+=======
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lb1.Text = "hello world!";
+>>>>>>> 48d03dd54f272bc63c06c4c91f4f6815e11a167f
         }
     }
 }
